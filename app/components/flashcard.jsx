63dd969d77ebd798a261a50a7b041e2931a7a75e
nate8kake <nathanielle411@gmail.com>
2024-11-09 22:08:15 -0600

    made flashcard

---
+++ app/components/flashcard.jsx
@@ -0,0 +1,7 @@
+export default function Flashcard() {
+  return (
+    <div>
+      <h1>Flashcardd</h1>
+    </div>
+  );
+} 
--- app/dictionary/page.jsx
+++ app/dictionary/page.jsx
@@ -1,7 +1,10 @@
+import Flashcard from "../components/flashcard";
+
 export default function Page(){
 	return(
 		<section>
 			<p>dictionary</p>
+			<Flashcard/>
 		</section>
 	);
 }
