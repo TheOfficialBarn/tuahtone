111e71f50b8abb3add726a017e8f1101a9b3e6a6
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-11-10 03:49:33 -0600

    Added SongView functionality

--- app/components/lyricsview.jsx
+++ app/components/lyricsview.jsx
@@ -1,71 +1,28 @@
-export default function LyricsView({songDetails}) {
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <h1>TITI ME PREGUNTO By Bad Bunny</h1>
-            <pre className="bg-songblockbackground rounded-xl px-8 py-4 w-3/4">
-{`[Letra de "Me Voy"]
-[Verso 1]
-¿Por qué no supiste entender a mi corazón?
-Lo que había en él ¿Por qué no tuviste el valor de ver quién soy?
-¿Por qué no escuchas lo que está tan cerca de ti?
-Sólo el ruido de afuera y yo
-Que estoy a un lado desaparezco para ti
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-[Pre-Coro]
-No voy a llorar y decir que no merezco esto
-Porque es probable que lo merezco
-Pero no lo quiero, por eso me voy
+export default function LyricsView({ track, artist }) {
+  const [lyrics, setLyrics] = useState('');
 
-[Coro]
-Qué lástima, pero adiós
-Me despido de ti y me voy
-Qué lástima, pero adiós
-Me despido de ti
+  useEffect(() => {
+    const fetchLyrics = async () => {
+      try {
+        const response = await axios.get(`https://lyrist.vercel.app/api/${track}/${artist}`);
+        setLyrics(response.data.lyrics);
+      } catch (error) {
+        console.error("Error fetching lyrics:", error);
+      }
+    };
 
-[Verso 2]
-Porque sé que me espera algo mejor
-Alguien que sepa darme amor
-De ese que endulza la sal y hace que salga el sol
-Yo que pensé, nunca me iría de ti
-Que es amor del bueno, de toda la vida, pero
-Hoy entendí que no hay suficiente para los dos
+    fetchLyrics();
+  }, [track, artist]);
 
-[Pre-Coro]
-No voy a llorar y decir que no merezco esto
-Porque es probable que lo merezco
-Pero no lo quiero, por eso me voy
-
-[Coro]
-Qué lástima, pero adiós
-Me despido de ti y me voy
-Qué lástima, pero adiós
-Me despido de ti y...
-
-[Puente]
-Voy
-Y me voy
-
-[Coro]
-Qué lástima, pero adiós
-Me despido de ti y me voy
-Qué lástima, pero adiós
-Me despido de ti y me voy
-
-[Outro]
-Qué lástima, pero adiós
-Me despido de ti y me voy
-Qué lástima, pero adiós
-Me despido de ti y me voy
-Mientes Tan Bien by Sin Bandera
-Ahora Te Puedes Marchar by Luis Miguel
-Corazón partío by Alejandro Sanz
-Inolvidable by Reik
-Perdón, Perdón - HA-ASH Primera Fila - Hecho Realidad [En Vivo] by Ha*Ash
-Sálvame by RBD, Anahí, Dulce María, Maite Perroni, Christian Chávez, Christopher von Uckermann, Alfonso Herrera
-No Querías Lastimarme by Gloria Trevi
-Inevitable by Shakira
-La De La Mala Suerte by Jesse & Joy`}
-            </pre>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>{track} By {artist}</h1>
+      <pre className="bg-songblockbackground rounded-xl px-8 py-4 w-3/4">
+        {lyrics}
+      </pre>
+    </div>
+  );
 }
