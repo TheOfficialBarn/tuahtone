87d40540df0237970316333881669e6ef9ed67a4
flec1115 <balandranf11@gmail.com>
2024-11-10 03:35:11 -0600

    created authcontext to be able to see if logged in or not to allow user acess

--- app/dictionary/page.jsx
+++ app/dictionary/page.jsx
@@ -1,7 +1,22 @@
-export default function Page(){
-	return(
-		<section>
-			<h1>Dictionary: Your Song Word Bank</h1>
-		</section>
-	);
+// app/dictionary/page.jsx
+"use client";
+
+import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import Link from 'next/link';
+
+export default function Page() {
+  const { user } = useAuth();
+
+  return (
+    <section>
+      {user ? (
+        <h1>Dictionary: Your Song Word Bank</h1>
+      ) : (
+        <>
+          <h1>Log In to Access Your Dictionary</h1>
+        </>
+      )}
+    </section>
+  );
 }
